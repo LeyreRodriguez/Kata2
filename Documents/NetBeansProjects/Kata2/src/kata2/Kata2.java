27120a7ef5a907024533d49a535ddb9e07b5bf82
Entrar <Entrar@PC-1167>
2021-10-07 11:25:30 +0100

    Kata 2 version1

---
+++ Documents/NetBeansProjects/Kata2/src/kata2/Kata2.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kata2;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Kata2 {
+
+    public static void main(String[] args) {
+        //Map<Integer, Integer> data = new HashMap<Integer, Integer>();
+        int[] data = {4,2,3,6,8,4,7,8,5,6,7,4,8,5,0};
+        
+        
+        Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
+        
+        
+        for (int i = 0; i < data.length; i++) {
+            
+            
+            if (histogram.keySet().contains(data[i] )){
+                histogram.put(data[i], histogram.get(data[i]) +1);                
+            }else{
+                histogram.put(data[i],1);
+            }
+            
+        }
+        
+        for(Integer key : histogram.keySet()){
+            System.out.println(key + "==>" + histogram.get(key));
+        }
+    }
+}
+        
