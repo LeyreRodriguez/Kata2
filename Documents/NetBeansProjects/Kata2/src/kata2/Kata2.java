1b1cad27ebb3ac142320c68761328a828c6a6cf8
Entrar <Entrar@PC-1167>
2021-10-07 11:49:23 +0100

    Kata 2 version2

--- Documents/NetBeansProjects/Kata2/src/kata2/Kata2.java
+++ Documents/NetBeansProjects/Kata2/src/kata2/Kata2.java
@@ -18,21 +18,10 @@ public class Kata2 {
         
         Map<Integer, Integer> histogram = new HashMap<Integer, Integer>();
         
-        
-        
-        
-        for (int i = 0; i < data.length; i++) {
-            
-           
-            if (histogram.keySet().contains(data[i] )){
-                histogram.put(data[i], histogram.get(data[i]) +1);                
-            }else{
-                histogram.put(data[i],1);
-            }
-
+        for (int key : data) {
+            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
         }
-        
-      
+
         for(Integer key : histogram.keySet()){
             System.out.println(key + "==>" + histogram.get(key));
         }
